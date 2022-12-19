e7a4c9d5ab551073cee3f01254dad652d02cb42c
micha <michal.jeleniasty@gmail.com>
2022-12-19 16:12:43 +0100

    initial commit

---
+++ src/main/java/com/jeleniasty/passwordgenerator/PasswordGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.jeleniasty.passwordgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordGeneratorApplication.class, args);
+	}
+
+}
